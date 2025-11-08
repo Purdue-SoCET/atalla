148f5ef73a05164a280d0780257b7e40397eacf4
Joseph <sowersj@purdue.edu>
2025-11-08 16:39:19 -0500

    lfc_wr_sequence created... having issues with it though

---
+++ uvm/lfc_wr_sequence.sv
@@ -0,0 +1,73 @@
+`ifndef LFC_WR_SEQUENCE_SV
+`define LFC_WR_SEQUENCE_SV
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "lfc_cpu_transaction.sv"
+
+
+class lfc_wr_sequence extends uvm_sequence#(lfc_cpu_transaction);
+  `uvm_object_utils(lfc_wr_sequence)
+  parameter NUM_TRANSACTIONS = 30;
+
+  function new(string name = "lfc_wr_sequence");
+    super.new(name);
+  endfunction
+
+  virtual task body();
+    lfc_cpu_transaction req;
+    logic [31:0] addrs [NUM_TRANSACTIONS];   
+    int addrs_idx;
+
+    `uvm_info(get_type_name(), "Starting lfc_wr_sequence...", UVM_MEDIUM)
+
+    req = lfc_cpu_transaction::type_id::create("req");
+
+    `uvm_info(get_type_name(), "Starting write transactions...", UVM_MEDIUM)
+    addrs_idx = 0;
+    repeat(NUM_TRANSACTIONS) begin // write transactions
+        start_item(req);
+        if(!req.randomize()) begin
+            `uvm_fatal("lfc_wr_sequence", "Not able to randomize")
+        end
+        req.n_rst = 1'b1;
+        req.mem_in_rw_mode = 1'b1; // 1 for write, 0 for read
+        req.mem_in = 1'b0; // understanding is that this is 1 for a read??
+        req.dp_in_halt = 1'b0;
+        addrs[addrs_idx] = req.mem_in_addr;
+        finish_item(req);
+
+        `uvm_info(get_type_name(),
+        $sformatf("Completed: addr=0x%0h data=0x%0h", req.mem_in_addr, req.mem_in_store_value),
+        UVM_LOW)
+
+        addrs_idx++;
+    end
+
+    `uvm_info(get_type_name(), "Starting read transactions...", UVM_MEDIUM)
+    addrs_idx = 0;
+    repeat(NUM_TRANSACTIONS) begin // read transactions
+        start_item(req);
+        if(!req.randomize()) begin
+            `uvm_fatal("lfc_wr_sequence", "Not able to randomize")
+        end
+        req.n_rst = 1'b1;
+        req.mem_in_rw_mode = 1'b0; // 1 for write, 0 for read
+        req.mem_in = 1'b1; // understanding is that this is 1 for a read??
+        req.dp_in_halt = 1'b0;
+        req.mem_in_addr = addrs[addrs_idx];
+        finish_item(req);
+
+        `uvm_info(get_type_name(),
+        $sformatf("Completed: addr=0x%0h data=0x%0h", req.mem_in_addr, req.mem_in_store_value),
+        UVM_LOW)
+
+        addrs_idx++;
+    end
+
+  endtask
+endclass
+
+
+
+`endif // LFC_WR_SEQUENCE_SV
--- uvm/test.sv
+++ uvm/test.sv
@@ -1,13 +1,16 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 `include "lfc_environment.sv"
+`include "lfc_basic_sequence.sv"
+`include "lfc_wr_sequence.sv"
 
 class test extends uvm_test;
     `uvm_component_utils(test)
 
     lfc_environment env;
     virtual lfc_if vif;
-    lfc_basic_sequence seq;
+    lfc_basic_sequence basic_seq;
+    lfc_wr_sequence wr_seq;
 
     function new(string name = "test", uvm_component parent);
         super.new(name, parent);
@@ -17,7 +20,8 @@ class test extends uvm_test;
         `uvm_info("Test", "Build Phase", UVM_LOW)
 
         env = lfc_environment::type_id::create("env", this);
-        seq = lfc_basic_sequence::type_id::create("seq", this);
+        basic_seq = lfc_basic_sequence::type_id::create("basic_seq", this);
+        wr_seq = lfc_wr_sequence::type_id::create("wr_seq", this);
 
         // Retrieve and send interface down
         if (!uvm_config_db#(virtual lfc_if)::get(this, "", "lfc_vif", vif)) begin
@@ -31,7 +35,9 @@ class test extends uvm_test;
     task run_phase(uvm_phase phase);
         phase.raise_objection(this, "Starting sequence in main phase");
         $display("%t Starting sequence run_phase", $time);
-        seq.start(env.cpu_active_agent.sqr);
+        basic_seq.start(env.cpu_active_agent.sqr);
+        #100ns;
+        wr_seq.start(env.cpu_active_agent.sqr);
         #100ns;
         phase.drop_objection(this, "Finished in main phase");
     endtask
