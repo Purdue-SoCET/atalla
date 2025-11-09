e8e9a383dfd4c05a343262d75155aa0aa50531ad
Jacob Thomas Walter <walte250@asicfab.ecn.purdue.edu>
2025-11-09 12:44:52 -0500

    fp16_sqrt moved and starting on bf166_sqrt

---
+++ rtl/modules/common/arithmetic/sqrt_bf16/sqrt_bf16.sv
@@ -0,0 +1,13 @@
+`include "sqrt_types.vh"
+`include "sqrt_fp16_if.sv"
+
+module sqrt_bf16 (
+    input logic         CLK,
+    input logic         nRST,
+    sqrt_if.srif        srif
+)
+
+    
+
+
+endmodule
