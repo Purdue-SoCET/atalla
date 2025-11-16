6bd4e50bafdc40cb49140841c8a16a8013f02ad2
Joseph <sowersj@purdue.edu>
2025-11-15 22:33:32 -0500

    added formatted waveforms config

--- lockup_free_cache/lockup_free_cache.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/obj/xsim_25.c
+++ lockup_free_cache/lockup_free_cache.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/obj/xsim_25.c
@@ -110,7 +110,7 @@ namespace XILINX_DPI {
 		DPIAllocateDPIScopes(1) ;
 		DPISetDPIScopeFunctionName(0, "m__uvm_report_dpi") ;
 		DPISetDPIScopeHierarchy(0, "uvm_pkg") ;
-		DPIRelocateDPIScopeIP(0, (char*)(0x5070f0)) ;
+		DPIRelocateDPIScopeIP(0, (char*)(0x507090)) ;
 		DPIAddDPIScopeAccessibleFunction(0, 0) ;
 	}
 
@@ -153,7 +153,7 @@ extern "C" {
 		int callingProcessOffset = topOffset() ;
 		char* SP ;
 		void* oldSPcontext = DPIGetCurrentContext();
-		SP = DPIInitializeFunction(IP + 418032, 176, 633680) ;
+		SP = DPIInitializeFunction(IP + 418032, 176, 1005896) ;
 		char driver0[32] ;
 		for(int i = 0 ; i < 32 ; ++i) driver0[i] = 0 ;
 		char driver1[32] ;
