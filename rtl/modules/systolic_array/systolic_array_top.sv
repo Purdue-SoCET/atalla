6354f29780419a7e97625d10ca8ce69b2849097d
mylesqpurdue <195245736+mylesqpurdue@users.noreply.github.com>
2025-11-16 16:28:08 -0500

    initial push systolic_array_top (might be finished) started to work on the test bench hahahahahaha

--- rtl/modules/systolic_array/systolic_array_top.sv
+++ rtl/modules/systolic_array/systolic_array_top.sv
@@ -14,29 +14,19 @@ module systolic_array(
     input logic clk, nRST,
     systolic_array_if.memory_array memory
 );
-    // Input to systolic array
-    logic [DW*N-1:0] top_input;
-    // logic [DW*N-1:0] weights_input;
-    // logic [DW*N-1:0] partial_sums;
-    // Load signals within systolic array
-    logic [N-1:0] loadi;
-    // logic [N-1:0] loadw;
-    logic [N-1:0] loadps;
     // MAC Unit inputs/outputs latched within systolic array
     logic [DW-1:0] MAC_inputs [N-1:0][N-1:0];
     logic weight_enables [N-1:0] [N-1:0];
     logic [DW-1:0] MAC_outputs [N-1:0][N-1:0];
     logic [DW-1:0] nxt_MAC_outputs [N-1:0][N-1:0];
-    // Partial Sum adder inputs
-    logic [DW-1:0] ps_add_inputs [N-1:0];
-    // Weight Registers
-    // logic [DW*N-1:0] weights [N-1:0];
+    
+    // Partial sum buffer inputs (connected to top row adders)
+    logic [DW-1:0] psum_buffer_inputs [N-1:0];
 
     // Generate variables
-    genvar /*i,*/j,l,m,n,o,p;
+    genvar j,m,n,o,p;
 
-    // Instantiate Control Unit interface    always_comb begin
-
+    // Instantiate Control Unit interface
     systolic_array_control_unit_if control_unit_if();
 
     // Instantiate the control unit
@@ -51,12 +41,9 @@ module systolic_array(
     systolic_array_MAC_if mac_ifs[N*N-1:0] (); 
     // Instantiate partial sum adder interfaces
     systolic_array_add_if add_ifs[N-1:0] (); 
-    // Instantiate Input Fifos
-    systolic_array_FIFO_if input_fifos_ifs[N-1:0] (); 
-    // Instantiate Partial Fifos
-    systolic_array_FIFO_if ps_fifos_ifs[N-1:0] (); 
     // Instantiate Output Fifos
     systolic_array_OUT_FIFO_if out_fifos_ifs[N-1:0] (); 
+    
     always_comb begin : control_unit_connections
         control_unit_if.weight_en = memory.weight_en;
         control_unit_if.input_en = memory.input_en;
@@ -65,47 +52,24 @@ module systolic_array(
         control_unit_if.row_ps_en = memory.row_ps_en;
         memory.fifo_has_space = control_unit_if.fifo_has_space;
     end
-    //Selection Muxes for the input bus
-    assign top_input = memory.array_in;                  // Temporarily bypassing inputs/weights bus selection to test loading weights into MAC units directly
-   
-    always_comb begin : load_sel
-        loadi = '0;
-        // loadw = '0;
-        loadps = '0;
-        if (control_unit_if.input_load == 1'b1) begin
-            loadi[control_unit_if.input_row] = 1'b1;
-        end
-        if (control_unit_if.partials_load == 1'b1) begin
-            loadps[control_unit_if.partials_row] = 1'b1;
-        end
-    end
     
-    // Input Fifo Generation
+    // Direct input connection - take values immediately from array_in
+    // Each row gets its corresponding slice of the input bus
+    // Data streams in and computation starts immediately (no buffering)
     generate
-        for (j = 0; j < N; j++) begin
-            sysarr_FIFO i_fifo (
-                .clk(clk),
-                .nRST(nRST),
-                .fifo(input_fifos_ifs[j].FIFO));
-            assign input_fifos_ifs[j].load = loadi[j];
-            assign input_fifos_ifs[j].shift = control_unit_if.in_fifo_shift[j];
-            assign input_fifos_ifs[j].load_values = top_input;
-            assign MAC_inputs[j][0] = memory.weight_en ? memory.array_in[((N-j)*DW)-1 : ((N-j-1)*DW)] : input_fifos_ifs[j].out;
+        for (j = 0; j < N; j++) begin : input_connections
+            // Directly drive front column from array_in
+            assign MAC_inputs[j][0] = memory.array_in[((N-j)*DW)-1 : ((N-j-1)*DW)];
             assign weight_enables[j][0] = memory.weight_en;
         end
     endgenerate
 
-    // Partial Sum Generation
+    // Partial sum buffer connection - connect to top row adders
+    // hey vinay this needs to be connected to an actual input buffer i think
+    // For now, using array_in_partials as the psum buffer source
     generate
-        for (l = 0; l < N; l++) begin
-            sysarr_FIFO ps_fifos (
-                .clk(clk),
-                .nRST(nRST),
-                .fifo(ps_fifos_ifs[l].FIFO));
-            assign ps_fifos_ifs[l].load = loadps[l];
-            assign ps_fifos_ifs[l].shift = control_unit_if.ps_fifo_shift[l];
-            assign ps_fifos_ifs[l].load_values = memory.array_in_partials;
-            assign ps_add_inputs[l] = ps_fifos_ifs[l].out;
+        for (j = 0; j < N; j++) begin : psum_buffer_connections
+            assign psum_buffer_inputs[j] = memory.array_in_partials[((N-j)*DW)-1 : ((N-j-1)*DW)];
         end
     endgenerate
 
@@ -118,14 +82,14 @@ module systolic_array(
                     MAC_outputs[z][y] <= '0;
                 end
             end
-        end else if (control_unit_if.MAC_value_ready == 1'b1)begin //add params later
+        end else if (control_unit_if.MAC_value_ready == 1'b1)begin
             MAC_outputs <= nxt_MAC_outputs;
         end 
     end
 
     generate
-        for (m = 0; m < N; m++) begin
-            for (n = 0; n < N; n++) begin
+        for (m = 0; m < N; m++) begin : mac_row_gen
+            for (n = 0; n < N; n++) begin : mac_col_gen
                 sysarr_MAC mac_inst (
                     .clk(clk),
                     .nRST(nRST),
@@ -134,18 +98,22 @@ module systolic_array(
                 if (m==0 && n==0) begin : mac_ready
                     assign control_unit_if.MAC_value_ready = mac_ifs[m*N + n].value_ready;
                 end
+                // Start computation immediately when data arrives
                 assign mac_ifs[m*N + n].start = control_unit_if.MAC_start;
-                // assign mac_ifs[m*N + n].weight = weights[n][(N - m) * DW - 1 -: DW];
-                // assign mac_ifs[m*N + n].weight_en = memory.weight_en;
                 assign mac_ifs[m*N + n].in_value = MAC_inputs[m][n];
                 assign mac_ifs[m*N + n].weight_en = weight_enables[m][n];
                 assign mac_ifs[m*N + n].MAC_shift = control_unit_if.MAC_shift;
                 assign mac_ifs[m*N + n].stall_sa = memory.stall_sa;
+                
+                // Top row (m==0): no accumulation from previous row
                 if (m == 0) begin : no_accumulate
                     assign mac_ifs[m*N + n].in_accumulate = '0;
                 end else begin : accumulation_blk
+                    // Accumulate from previous row
                     assign mac_ifs[m*N + n].in_accumulate = MAC_outputs[m-1][n];
                 end
+                
+                // Horizontal data forwarding (systolic flow)
                 if (n != 0)begin : macInputForwarding
                     assign MAC_inputs[m][n] = mac_ifs[m*N + (n-1)].in_pass;
                     assign weight_enables[m][n] = mac_ifs[m*N + (n-1)].weight_next_en;
@@ -156,18 +124,21 @@ module systolic_array(
     endgenerate
 
     // Partial Sum Output Adders Generation
+    // Connected to top row (row 0) of MAC units for psum accumulation
     generate
-        for (o = 0; o < N; o++) begin
+        for (o = 0; o < N; o++) begin : adder_gen
             sysarr_add add_inst (
                 .clk(clk),
                 .nRST(nRST),
-                .adder(add_ifs[o].add)//weights
+                .adder(add_ifs[o].add)
             );
             if (o == 0) begin : add_ready
                 assign control_unit_if.add_value_ready = add_ifs[o].value_ready;
             end
             assign add_ifs[o].start = control_unit_if.add_start;
-            assign add_ifs[o].add_input1 = ps_add_inputs[o];
+            // Connect psum buffer to adder input1 (top row accumulation)
+            assign add_ifs[o].add_input1 = psum_buffer_inputs[o];
+            // Connect bottom row MAC output to adder input2
             assign add_ifs[o].add_input2 = MAC_outputs[N-1][o];
         end
     endgenerate
