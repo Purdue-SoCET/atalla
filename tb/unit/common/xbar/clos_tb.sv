edec1477ab39f2446e5bf550534699f7a9d87f73
Haejune Kwon <kwon196@asicfab.ecn.purdue.edu>
2025-12-05 15:01:39 -0500

    updated clos for en

--- tb/unit/common/xbar/clos_tb.sv
+++ tb/unit/common/xbar/clos_tb.sv
@@ -25,7 +25,8 @@ module clos_tb;
         n_rst = 0;
         val = 16'd0;
         perm = {5'd14, 5'd22, 5'd25, 5'd11, 5'd21, 5'd6, 5'd15, 5'd5, 5'd30, 5'd23, 5'd18, 5'd28, 5'd19, 5'd17, 5'd31, 5'd12, 5'd26, 5'd16, 5'd13, 5'd3, 5'd9, 5'd8, 5'd0, 5'd1, 5'd10, 5'd20, 5'd7, 5'd4, 5'd29, 5'd2, 5'd24, 5'd27};
-
+        xif.en = 1;
+        
         for (i = 0; i < 32; i = i + 1) begin
             xif.in[i].din = val;
             xif.in[i].shift = perm[i];
