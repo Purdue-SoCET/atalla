8b5cff337f7c741a3f4b0d3a22b1ac3b66372ae9
Jacob Thomas Walter <walte250@asicfab.ecn.purdue.edu>
2025-12-09 16:57:38 -0500

    final sqrt and reduction pushed

--- rtl/modules/vector/reduction/vreduction.sv
+++ rtl/modules/vector/reduction/vreduction.sv
@@ -13,11 +13,11 @@ module vreduction #(
     
     logic busy, broadcast_registered, clear_registered;
     logic [4:0] imm_registered;
-    logic [NUM_ELEMENTS-1:0][15:0] vector_registered, output_vector, output_vector_n;
+    logic [1:0] reduction_type_registered;
+    logic [NUM_ELEMENTS-1:0][15:0] vector_registered, output_vector;
     logic valid_out_reg;
 
     //we are ready if ready_out, and not ready until we output valid data again
-
     always_ff @(posedge CLK, negedge nRST) begin
         if (!nRST) begin
             busy <= 1'b0;
@@ -36,13 +36,14 @@ module vreduction #(
         vruif.out.ready_in = (!busy) & vruif.in.ready_out;
     end
 
-    //on a valid input, latch the input vector
+    //on a valid input, latch all control signals and data
     always_ff @(posedge CLK, negedge nRST) begin
         if (!nRST) begin
             vector_registered <= 'b0;
             broadcast_registered <= 'b0;
             clear_registered <= 'b0;
             imm_registered <= 'b0;
+            reduction_type_registered <= 'b0;
         end
         else begin
             if (vruif.in.valid_in & vruif.out.ready_in) begin
@@ -50,11 +51,12 @@ module vreduction #(
                 broadcast_registered <= vruif.in.broadcast;
                 clear_registered <= vruif.in.clear;
                 imm_registered <= vruif.in.imm;
+                reduction_type_registered <= vruif.in.reduction_type;
             end
         end
     end
 
-    //on a valid input, run the reduction tree
+    //run the reduction tree with registered alu_op
     logic [15:0] rtree_result;
     logic rtree_done;
     reduction_tree #(
@@ -63,44 +65,42 @@ module vreduction #(
         .CLK(CLK),
         .nRST(nRST),
         .data_in(vruif.in.lane_input),
-        .alu_op(vruif.in.reduction_type),
+        .alu_op(reduction_type_registered),  // Use registered version that stays stable
         .valid_in(vruif.in.valid_in & vruif.out.ready_in),
         .data_out(rtree_result),
         .valid_out(rtree_done)
     );
 
-    //when the tree is done, output masking and such
-    
+    //when the tree is done, update output vector
     always_ff @(posedge CLK, negedge nRST) begin
         if (!nRST) begin
             output_vector <= 'b0;
             valid_out_reg <= 1'b0;
         end
         else begin
-            output_vector <= output_vector_n;
-            valid_out_reg <= rtree_done;  // Delay valid by one cycle
-        end
-    end
-
-    always_comb begin
-        if (rtree_done) begin
-            for (int i = 0; i < NUM_ELEMENTS; i++) begin
-                if (clear_registered) begin
-                    output_vector_n[i] = (i == imm_registered) ? rtree_result : 'b0;
+            if (rtree_done) begin
+                // Update output_vector directly when tree is done
+                for (int i = 0; i < NUM_ELEMENTS; i++) begin
+                    if (clear_registered) begin
+                        output_vector[i] <= (i == imm_registered) ? rtree_result : 'b0;
+                    end
+                    else if (broadcast_registered) begin
+                        output_vector[i] <= rtree_result;
+                    end
+                    else begin
+                        output_vector[i] <= (i == imm_registered) ? rtree_result : vector_registered[i];
+                    end
                 end
-                else if (broadcast_registered) begin
-                    output_vector_n[i] = rtree_result;
-                end
-                else begin
-                    output_vector_n[i] = (i == imm_registered) ? rtree_result : vector_registered[i];
+                valid_out_reg <= 1'b1;
+            end
+            else begin
+                if (vruif.in.ready_out) begin
+                    valid_out_reg <= 1'b0;
                 end
             end
-        end
-        else begin
-            output_vector_n = output_vector;
         end
     end
 
     assign vruif.out.vector_output = output_vector;
-    assign vruif.out.valid_out = valid_out_reg;  // Use registered version
+    assign vruif.out.valid_out = valid_out_reg;
 endmodule
