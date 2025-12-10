5eb08591a56c32f07a2726c03b6406bf2c932751
Jacob Thomas Walter <walte250@asicfab.ecn.purdue.edu>
2025-12-10 14:02:11 -0500

    waiting on vector top to be done

--- tb/formal/vector/src/schedular.cpp
+++ tb/formal/vector/src/schedular.cpp
@@ -4,6 +4,7 @@ schedular::schedular()
 {
     clk = 0;
     rst_n = 0;
+    reset();
 }
 
 schedular::~schedular()
@@ -108,13 +109,13 @@ void schedular::decode(packet pkt)
     for (uint8_t i = 0; i < 2; i++)
     {
         instruction instr = pkt.inst[i];
-        bool add = (instr.opcode ==  50 | instr.opcode == 62 | instr.opcode == 71 | instr.opcode == 80);
-        bool subtract = (instr.opcode ==  51 | instr.opcode == 63 | instr.opcode == 81);
-        bool mult = (instr.opcode ==  52 | instr.opcode == 64 | instr.opcode == 82);
-        bool div = (instr.opcode == 53 | instr.opcode == 65 | instr.opcode == 83);
+        bool add = (instr.opcode == 50 || instr.opcode == 62 || instr.opcode == 71 || instr.opcode == 80);
+        bool subtract = (instr.opcode == 51 || instr.opcode == 63 || instr.opcode == 81);
+        bool mult = (instr.opcode == 52 || instr.opcode == 64 || instr.opcode == 82);
+        bool div = (instr.opcode == 53 || instr.opcode == 65 || instr.opcode == 83);
         bool exp = instr.opcode == 66;
         bool sqrt = instr.opcode == 67;
-        reduction_mode[i] == (instr.opcode == 71 | instr.opcode == 72 | instr.opcode == 73);
+        reduction_mode[i] = (instr.opcode == 71 || instr.opcode == 72 || instr.opcode == 73);
 
         if (add)
         {
@@ -157,12 +158,15 @@ void schedular::decode(packet pkt)
         {
             case S:
                 lane_valid_in[i] = 0;
+            break;
             case VV:
                 lane_vd[i] = instr.vd;
                 lane_vs1[i] = instr.vs1;
                 lane_vs2[i] = instr.vs2;
                 vmrf_vs[i] = instr.mask;
                 vmrf_mren[i] = 1;
+                lane_valid_in[i] = 1;
+                broadcast_v2[i] = 0;
             break;
             case VS:
                 lane_vd[i] = instr.vd;
@@ -171,6 +175,8 @@ void schedular::decode(packet pkt)
                 vmrf_vs[i] = instr.mask;
                 vmrf_mren[i] = 1;
                 std::fill(lane_v2_broadcast[i].begin(), lane_v2_broadcast[i].end(), broadcast_value);
+                broadcast_v2[i] = 1;
+                lane_valid_in[i] = 1;
             break;
             case VI:
                 lane_vd[i] = instr.vd;
@@ -181,6 +187,10 @@ void schedular::decode(packet pkt)
                 clear[i] = (instr.imm8 >> 6) & 0x1;
                 broadcast[i] = (instr.imm8 >> 7) & 0x1;
                 std::fill(lane_v2_broadcast[i].begin(), lane_v2_broadcast[i].end(), (instr.imm8 << 5) | instr.imm5);
+                broadcast_v2[i] = 1;
+                lane_valid_in[i] = 1;
+            break;
+            default:
             break;
             
         }
@@ -188,11 +198,73 @@ void schedular::decode(packet pkt)
     //end me
     //instr 3
     //only 4 possible operations here
+    instruction instr = pkt.inst[2];
+    if (instr.opcode == 57) //gemm
+    {
+        sys_vd = instr.vd;
+        sys_vs1 = instr.vs1;
+        sys_vs2 = instr.vs2;
+        sys_ren[0] = 1;
+        sys_ren[1] = 1;
+        sys_weight = 0;
+        sys_valid_in = 1;
+    }
+    else if (instr.opcode == 70) //lw
+    {
+        sys_vs1 = instr.vs1;
+        sys_ren[0] = 1;
+        sys_ren[1] = 0;
+        sys_weight = 1;
+        sys_valid_in = 1;
+    }
+    else if (instr.opcode == 77) //veggie load
+    {
+        sp_vd[0] = instr.vd;
+        sp_rs1[0] = instr.rs1;
+        sp_num_cols[0] = instr.num_cols;
+        sp_num_rows[0] = instr.num_rows;
+        sp_sid[0] = instr.sid;
+        sp_rc[0] = instr.rc;
+        sp_rcid[0] = instr.rc_id;
+        sp_valid_in[0] = 1;
 
+    }
+    else if (instr.opcode == 78) //veggie store
+    {
+        sp_valid_in[0] = 1;
+    }
+    else if (instr.opcode == 47)
+    {
+        sp_valid_in[0] = 0;
+        sys_valid_in = 0;
+        sys_ren[0] = 0;
+        sys_ren[1] = 0;
+    }
 
     //instr 4
-    //only 2 here
+    //only 3 here
     //its 1 AM fucking kill me please
+    instr = pkt.inst[3];
+    if (instr.opcode == 47)
+    {
+        sp_valid_in[1] = 0;
+    }
+    else if (instr.opcode == 77)
+    {
+        sp_vd[1] = instr.vd;
+        sp_rs1[1] = instr.rs1;
+        sp_num_cols[1] = instr.num_cols;
+        sp_num_rows[1] = instr.num_rows;
+        sp_sid[1] = instr.sid;
+        sp_rc[1] = instr.rc;
+        sp_rcid[1] = instr.rc_id;
+        sp_valid_in[1] = 1;
+    }
+    else if (instr.opcode == 78)
+    {
+        sp_valid_in[1] = 1;
+    }
+
 }
 
 void schedular::tick()
@@ -204,11 +276,27 @@ void schedular::tick()
         {
             std::queue<packet> q;
             std::swap(inst_queue, q); //clears the queue on reset
+            reset();
+            all_issued = false;
         }
         else {
-
+            deassert_valids();
+            if (all_ready()) //issue instruction
+            {
+                if (!inst_queue.empty())
+                {
+                    packet next_pkt = inst_queue.front();
+                    inst_queue.pop();
+                    decode(next_pkt);
+                }
+                else
+                {
+                    all_issued = true;
+                }
+            }
         }
     }
+    last_clk = clk;
 }
 
 
@@ -251,6 +339,8 @@ void schedular::dump_program_queue()
                 case S:
                     std::cout << " (NOP)";
                     break;
+                default:
+                    break;
             }
             std::cout << "\n";
         }
@@ -258,4 +348,115 @@ void schedular::dump_program_queue()
         temp_queue.pop();
         pkt_num++;
     }
+}
+
+void schedular::reset()
+{
+    lane_alu_ready = 0;
+    lane_exp_ready = 0;
+    lane_sqrt_ready = 0;
+    lane_mul_ready = 0;
+    lane_div_ready = 0;
+    sys_ready = 0;
+    sp_ready = 0;
+
+    // SC -> LN
+    lane_valid_in.fill(0);
+    broadcast_value = 0;
+    for (auto& arr : lane_v2_broadcast) {
+        arr.fill(0);
+    }
+    lane_vd.fill(0);
+    lane_op.fill(0);
+    fu_sel.fill(0);
+    reduction_mode.fill(0);
+    alu_op.fill(0);
+    imm8.fill(0);
+    imm5.fill(0);
+    broadcast.fill(0);
+    clear.fill(0);
+    reudction_imm.fill(0);
+
+    // SC -> SYS
+    sys_vd = 0;
+    sys_valid_in = 0;
+    sys_weight = 0;
+    sys_vs1 = 0;
+    sys_vs2 = 0;
+    sys_ren.fill(0);
+
+    // SC -> SP
+    sp_vd.fill(0);
+    sp_rs1.fill(0);
+    sp_num_cols.fill(0);
+    sp_num_rows.fill(0);
+    sp_sid.fill(0);
+    sp_rc.fill(0);
+    sp_rcid.fill(0);
+    sp_valid_in.fill(0);
+
+    // SC -> VEG
+    lane_ren.fill(0);
+    lane_vs1.fill(0);
+    lane_vs2.fill(0);
+    lane_rs1.fill(0);
+    
+    vmrf_vs.fill(0);
+    vmrf_mren.fill(0);
+}
+
+bool schedular::all_ready()
+{
+    
+    for (int i = 0; i < 4; i++)
+    {
+        instruction instr = inst_queue.front().inst[i];
+        if (i == 0 || i == 1)
+        {
+            bool add = (instr.opcode == 50 || instr.opcode == 62 || instr.opcode == 71 || instr.opcode == 80);
+            bool subtract = (instr.opcode == 51 || instr.opcode == 63 || instr.opcode == 81);
+            bool mult = (instr.opcode == 52 || instr.opcode == 64 || instr.opcode == 82);
+            bool div = (instr.opcode == 53 || instr.opcode == 65 || instr.opcode == 83);
+            bool exp = instr.opcode == 66;
+            bool sqrt = instr.opcode == 67;
+            bool reduction = (instr.opcode == 71 || instr.opcode == 72 || instr.opcode == 73);
+            if (add && !lane_alu_ready) {return false;}
+            if (subtract && !lane_alu_ready) {return false;}
+            if (mult && !lane_mul_ready) {return false;}
+            if (div && !lane_div_ready) {return false;}
+            if (exp && !lane_exp_ready) {return false;}
+            if (sqrt && !lane_sqrt_ready) {return false;}
+            if (reduction && !lane_alu_ready) {return false;}
+        }
+        else if (i == 2)
+        {
+            if (instr.sac)
+            {
+                if (instr.opcode == 57 && !sys_ready) {return false;} //gemm
+                else if (instr.opcode == 70 && !sys_ready) {return false;}//lw
+                else if (instr.opcode == 77 && !sp_ready) {return false;} //veggie load
+                else if (instr.opcode == 78 && !sp_ready) {return false;}//veggie store
+            }
+        }
+        else if (i == 3)
+        {
+            if (instr.opcode == 77 & !sp_ready) {return false;} //veggie load
+            else if (instr.opcode == 78 & !sp_ready) {return false;}//veggie store
+        }
+    }
+    return true;
+}
+
+
+void schedular::deassert_valids()
+{
+    lane_valid_in.fill(0);
+    sys_valid_in = 0;
+    sp_valid_in.fill(0);
+
+    lane_ren.fill(0);
+    vmrf_mren.fill(0);
+
+    sys_ren.fill(0);
+    
 }
