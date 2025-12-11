337b3ac846c3e912f5d0e61254c19a282bf30a98
AkshathRaghav <akshathraghav.r@gmail.com>
2025-12-10 20:08:46 -0500

    Storing
    Signed-off-by: AkshathRaghav <akshathraghav.r@gmail.com>

--- rtl/modules/common/memory/rom_bank.sv
+++ rtl/modules/common/memory/rom_bank.sv
@@ -1,20 +1,44 @@
 module rom_bank #(
   parameter int DWIDTH = 144,
-  parameter int DEPTH = 512,
-  parameter string INIT_FILE = "./controlbits.mem"
+  parameter int DEPTH  = 512
 ) (
-  input logic clk, en,
+  input logic clk,
+  input logic en,
   input logic [$clog2(DEPTH)-1:0] addr,
   output logic [DWIDTH-1:0] data
 );
-  logic [DWIDTH-1:0] mem [DEPTH];
+
+  // ROM array
+  logic [DWIDTH-1:0] dat [0:DEPTH-1];
+
+  // --------------------------------------------------
+  // SIMULATION-ONLY STUFF (hidden from synthesis)
+  // --------------------------------------------------
+`ifndef SYNTHESIS
+  // File name only needed in sim
+  localparam string INIT_FILE = "./controlbits.dat";
 
   initial begin
-    $readmemh(INIT_FILE, mem);
+    $readdath(INIT_FILE, dat);
+  end
+`endif
+
+  // --------------------------------------------------
+  // Optional: input/output FFs (synthesizable)
+  // --------------------------------------------------
+  logic                        en_q;
+  logic [$clog2(DEPTH)-1:0]    addr_q;
+
+  // Input pipeline stage
+  always_ff @(posedge clk) begin
+    en_q   <= en;
+    addr_q <= addr;
   end
 
-  always_ff @ (posedge clk) begin
-    if (en) data <= mem[addr];
+  // ROM read + output FF
+  always_ff @(posedge clk) begin
+    if (en_q)
+      data <= dat[addr_q];
   end
 
 endmodule
