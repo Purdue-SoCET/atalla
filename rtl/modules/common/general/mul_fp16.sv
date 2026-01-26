2cd30cc23b40c7afaf5f6647ad6d12cd9f84937e
MylesLinux <mylesquerimit@gmail.com>
2026-01-26 18:20:23 -0500

    extra latching (vector core mult)

--- rtl/modules/common/general/mul_fp16.sv
+++ rtl/modules/common/general/mul_fp16.sv
@@ -1,12 +1,12 @@
 `timescale 1ns/1ps
-// FP16 Single-cycle Multiplier with input latching
+// FP16 2 Multiplier with input and output latching
 // Wraps mul_fp16_nolatch with registered inputs for use in vector core.
 // For systolic array MAC use mul_fp16_nolatch directly.
 
 module mul_fp16(input logic clk, input logic nRST, input logic start, input logic [15:0] a, b, output logic [15:0] result, output logic done);
 
-    logic lat1_ready;                           // Signal to denote when the value is ready at the latch stage.
-    assign done = lat1_ready;                   // Mul result is ready when the value-latch register is ready - everything downstream of that is combinational.
+    logic lat1_ready, lat2_ready;                           // Signal to denote when the value is ready at the latch stage.
+    assign done = lat2_ready;                   // Mul result is ready when the value-latch register is ready - everything downstream of that is combinational.
 
     // Register 1: latch input values.
     logic [15:0] a_latched, b_latched;
@@ -30,6 +30,18 @@ module mul_fp16(input logic clk, input logic nRST, input logic start, input logi
     end
 
     // Instantiate the combinational multiplier core
-    mul_fp16_nolatch mul_core (.a(a_latched), .b(b_latched), .result(result));
+    logic [15:0] result_comb;
+    mul_fp16_nolatch mul_core (.a(a_latched), .b(b_latched), .result(result_comb));
 
+    // reg 2: latchout output 
+    always_ff @(posedge clk, negedge nRST) begin
+        if (nRST == 1'b0) begin
+            result <= 0;
+            lat2_ready <= 0;
+        end 
+        else begin
+            result <= result_comb;
+            lat2_ready <= lat1_ready;
+        end
+    end
 endmodule
