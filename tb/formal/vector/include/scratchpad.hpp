84c9eaf4029c238ff18364909a8e205768163951
Jacob Thomas Walter <walte250@asicfab.ecn.purdue.edu>
2026-01-30 12:41:47 -0500

    sysarr and sp refactor

--- tb/formal/vector/include/scratchpad.hpp
+++ tb/formal/vector/include/scratchpad.hpp
@@ -12,54 +12,48 @@
 class scratchpad {
     
     private:
-        Eigen::Matrix<Eigen::bfloat16, 32, 256> sp1;
-        Eigen::Matrix<Eigen::bfloat16, 32, 256> sp2; 
+        std::array<Eigen::Matrix<uint16_t, 32, 256>, 2> sp;
         
     public:
+        struct sp_input_if_t
+        {
+            uint8_t valid_in;
+            uint8_t wen;
+            uint8_t addr;
+            uint8_t num_rows;
+            uint8_t num_cols;
+            uint8_t row_id;
+            uint8_t col_id;
+            uint8_t row_or_col;
+            std::array<uint16_t, 32> wdata;
+        };
+
+        struct sp_output_if_t
+        {
+            uint8_t valid;
+            std::array<uint16_t, 32> rdata;
+        };
+        
+
         uint8_t clk;
         uint8_t rst_n;
-        std::queue<int> read_completion_cycles_sp1;
-        std::queue<int> read_completion_cycles_sp2;
+        std::array<std::queue<int>, 2> completion_cycles;
+        std::array<std::queue<std::array<uint16_t, 32>>, 2> wb_queue;
         int cycle_count = 0;
-        //input
-        uint8_t valid_in_sp1;
-        uint8_t write_sp1;
-        uint8_t sp_addr_sp1;
-        uint8_t num_rows_sp1;
-        uint8_t num_cols_sp1;
-        uint8_t row_id_sp1;
-        uint8_t col_id_sp1;
-        uint8_t row_or_col_sp1;
-        std::array<uint16_t, 32> wdata_sp1; 
-        //output
-        uint8_t valid_out_sp1;
-        std::array<uint16_t, 32> rdata_sp1;
-
-        std::queue<Eigen::Matrix<Eigen::bfloat16, 32, 1>> wb_queue_sp1;
-
-        uint8_t valid_in_sp2;
-        uint8_t write_sp2;
-        uint8_t sp_addr_sp2;
-        uint8_t num_rows_sp2;
-        uint8_t num_cols_sp2;
-        uint8_t row_id_sp2;
-        uint8_t col_id_sp2;
-        uint8_t row_or_col_sp2;
-        std::array<uint16_t, 32> wdata_sp2; 
-        //output
-        uint8_t valid_out_sp2;
-        std::array<uint16_t, 32> rdata_sp2;
-
-        std::queue<Eigen::Matrix<Eigen::bfloat16, 32, 1>> wb_queue_sp2;
+        
+        std::array<sp_input_if_t, 2> sp_input_if;
+        std::array<sp_output_if_t, 2> sp_output_if;
+        std::queue<std::array<uint16_t, 32>> wb_queue_sp1;
+        std::queue<std::array<uint16_t, 32>> wb_queue_sp2;        
 
         scratchpad();
 
         void tick();
 
-        void load_row(uint8_t sp_id, uint8_t addr, uint8_t row_id, uint8_t num_cols, Eigen::Matrix<Eigen::bfloat16, 32, 1> wdata);
-        void load_col(uint8_t sp_id, uint8_t addr, uint8_t col_id, uint8_t num_rows, Eigen::Matrix<Eigen::bfloat16, 32, 1> wdata);
-        Eigen::Matrix<Eigen::bfloat16, 32, 1> read_row(uint8_t sp_id, uint8_t addr, uint8_t row_id, uint8_t num_cols);
-        Eigen::Matrix<Eigen::bfloat16, 32, 1> read_col(uint8_t sp_id, uint8_t addr, uint8_t col_id, uint8_t num_rows);
+        void load_row(uint8_t sp_id, uint8_t addr, uint8_t row_id, uint8_t num_cols, std::array<uint16_t, 32> wdata);
+        void load_col(uint8_t sp_id, uint8_t addr, uint8_t col_id, uint8_t num_rows, std::array<uint16_t, 32> wdata);
+        std::array<uint16_t, 32> read_row(uint8_t sp_id, uint8_t addr, uint8_t row_id, uint8_t num_cols);
+        std::array<uint16_t, 32> read_col(uint8_t sp_id, uint8_t addr, uint8_t col_id, uint8_t num_rows);
     };
 
 
