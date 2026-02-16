d0538daed2a8c3ccbdccfee712dd883e08bb52ef
Nikhil Kishore Vaidyanath <nvaidyan@asicfab.ecn.purdue.edu>
2026-02-16 14:12:06 -0500

    added reverse triangle

--- rtl/modules/systolic_array/skew_buffer.sv
+++ rtl/modules/systolic_array/skew_buffer.sv
@@ -7,9 +7,11 @@ module skew_buffer #(
     parameter int COL_WIDTH         = 16,   // same as bank width (since one bank per column)
     parameter int RECT_DELAY        = 4,    // base level of delay for EVERY column
     parameter int DELAY_SLOPE       = 1,    // how much delay/skew from one column to next (in addition to rectangle delay)
-    parameter int READ_LATENCY      = 1,    // sram read latency
-    parameter int WRITE_LATENCY     = 1,    // sram write latency (MUST be equal to read latency)
+    parameter logic REVERSE_TRIANGLE = 0,   // If 1, reverse triangle direction (right is lowest, left is highest)
     
+    // assumption of 1 cycle read/writes
+    localparam int READ_LATENCY      = 1,    // sram read latency
+    localparam int WRITE_LATENCY     = 1,    // sram write latency (MUST be equal to read latency)
     // since first column gets 0 delay, last column can get NUM_COLS - 1 delay coefficient
     localparam int SRAM_DEPTH = RECT_DELAY + (DELAY_SLOPE * (NUM_COLS - 1)),
     localparam int PTR_WIDTH = $clog2(SRAM_DEPTH)
@@ -18,63 +20,60 @@ module skew_buffer #(
     input  logic stall,
 
     // Write side
-    input  logic                                    wr_en,
     input  logic [NUM_COLS-1:0][COL_WIDTH-1:0]      wr_data,
 
     // Read side
     output logic [NUM_COLS-1:0][COL_WIDTH-1:0]      rd_data,
 );
 
-
-
     // use global read/write pointers, offset using delay slope and initialize using latencies
     logic [PTR_WIDTH-1:0] wr_ptr, rd_ptr, next_wr_ptr, next_rd_ptr;
 
-    // this only matters if latency is over 1, we need to hold address for that many cycles,
-    // ends up making rd/wr_ptr not advance every non-stalled cycle
-    logic [$clog2(READ_LATENCY):0] ptr_counter, next_ptr_counter;
-
     always_ff @(posedge clk, negedge n_rst) begin : pointer_regs
         if (~n_rst) begin
-            wr_ptr <= RECT_DELAY - READ_LATENCY; // initialize the skew in write pointer
+            wr_ptr <= RECT_DELAY; // initialize the skew in write pointer
             rd_ptr <= '0;
-            ptr_counter <= '0;
         end else begin
             wr_ptr <= next_wr_ptr;
             rd_ptr <= next_rd_ptr;
-            ptr_counter <= next_ptr_counter;
         end
     end
 
     always_comb begin : next_pointer
         next_wr_ptr = wr_ptr;
         next_rd_ptr = rd_ptr;
-        next_ptr_counter = ptr_counter;
 
         // we're ALWAYS incrementing read & write pointers (when latency allows us to) UNLESS stall is high
         if (!stall) begin
             if (wr_ptr >= SRAM_DEPTH - 1) begin
                 next_wr_ptr = '0;
-            end else if (ptr_counter == READ_LATENCY - 1) begin
-                next_wr_ptr = wr_ptr + 1;
-                next_ptr_counter = '0;
             end else begin
-                next_ptr_counter = ptr_counter + 1;
+                next_wr_ptr = wr_ptr + 1;
             end
 
             if (rd_ptr >= SRAM_DEPTH - 1) begin
                 next_rd_ptr = '0;
-            end else if (ptr_counter == READ_LATENCY - 1) begin
-                next_rd_ptr = rd_ptr + 1;
-                next_ptr_counter = '0;
             end else begin
-                next_ptr_counter = ptr_counter + 1;
+                next_rd_ptr = rd_ptr + 1;
             end
         end
     end
 
+    logic [$clog2(SRAM_DEPTH):0] waddr;
+
     generate;
         for (genvar i; i < NUM_COLS, i++) begin
+            // Reverse Triangle logic, flip the direction of the slope
+            if (REVERSE_TRIANGLE) begin
+                waddr = wr_ptr + (DELAY_SLOPE * (NUM_COLS - i - 1));
+            end else begin
+                waddr = wr_ptr + (DELAY_SLOPE * i);
+            end
+
+            if (waddr >= SRAM_DEPTH) begin
+                waddr = waddr - SRAM_DEPTH;
+            end
+
             sram_bank #(
                 .READ_LATENCY  (READ_LATENCY),
                 .WRITE_LATENCY (WRITE_LATENCY),
@@ -88,12 +87,12 @@ module skew_buffer #(
                 .busy  (),
                 .ren   (!stall),
                 .raddr (rd_ptr), // NO offset for the rd_ptr
-                .rdata (rd_data),
+                .rdata (rd_data[i]),
                 .rdone (),
 
                 .wen   (!stall),
-                .waddr (wr_ptr + (DELAY_SLOPE * i)),
-                .wdata (wr_data),
+                .waddr (waddr),
+                .wdata (wr_data[i]),
                 .wdone ()
             );
         end
