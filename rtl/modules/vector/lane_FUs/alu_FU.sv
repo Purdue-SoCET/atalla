a965412ba610fd922ff3ccb6134ac29c0429a501
Jacob Thomas Walter <walte250@asicfab.ecn.purdue.edu>
2026-02-17 13:21:04 -0500

    div and exp FU done

---
+++ rtl/modules/vector/lane_FUs/alu_FU.sv
@@ -0,0 +1,8 @@
+// ------------------------------------------------------------
+// alu_FU.sv
+// Lane FU for alu
+// Owner: Jacob Walter
+// Paramater control for this file is in vector_pkg.vh
+// ------------------------------------------------------------
+`include "vector_pkg.vh"
+`include "functional_unit_if.vh"
