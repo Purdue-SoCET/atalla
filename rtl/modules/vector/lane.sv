0ba3d0a1765c3cba7fa85e41e2e53056e32b642b
Jacob Thomas Walter <walte250@asicfab.ecn.purdue.edu>
2026-02-18 16:04:52 -0500

    preliminary vector datapath

--- rtl/modules/vector/lane.sv
+++ rtl/modules/vector/lane.sv
@@ -3,7 +3,7 @@
 // Lane wrapper: Lane unit
 // Owner: Jacob Walter
 // ------------------------------------------------------------
-`include "lane_in.vh"
+`include "lane_if.vh"
 `include "functional_unit_if.vh"
 `include "vector_pkg.vh"
 
@@ -19,26 +19,32 @@ module lane #(
 
     //Functional Unit instanciation
     functional_unit_if fu_interfaces [LANE_FU_COUNT]();
+    /*
     alu_FU alu (
         .CLK(CLK),
         .nRST(nRST),
         .fuif(fu_interfaces[0])
     );
-    mult_FU mult (
+    */
+    mul_FU mult (
         .CLK(CLK),
         .nRST(nRST),
         .fuif(fu_interfaces[1])
     );
+    /*
     div_FU div (
         .CLK(CLK),
         .nRST(nRST),
         .fuif(fu_interfaces[2])
     );
+    */
+    /*
     exp_FU exp (
         .CLK(CLK),
         .nRST(nRST),
         .fuif(fu_interfaces[3])
     );
+    */
     sqrt_FU sqrt (
         .CLK(CLK),
         .nRST(nRST),
@@ -47,24 +53,24 @@ module lane #(
 
 
     //connect the two input ports to every single FU
-    always_comb begin : input_bus_connection
-        for (int i = 0; i < LANE_FU_COUNT; i++) begin //iterate over each FU's interface
-            for (int j = 0; j < LANE_ISSUE_W; j++) begin //iterate over all input ports
-                fu_interfaces[i].in.ports[j].input_valid =  lif.in.input_valid[j];
-                fu_interfaces[i].in.ports[j].v1 =           lif.in.v1[j];
-                fu_interfaces[i].in.ports[j].v2 =           lif.in.v2[j];
-                fu_interfaces[i].in.ports[j].usel =         lif.in.usel[j];
-                fu_interfaces[i].in.ports[j].vd =           lif.in.vd[j];
-                fu_interfaces[i].in.ports[j].rm =           lif.in.rm[j];
-                fu_interfaces[i].in.ports[j].mask =         lif.in.mask[j];
-                fu_interfaces[i].in.ports[j].alu_op =       lif.in.aluop[j];
-                
+    genvar i, j;
+    generate
+        for (i = 0; i < LANE_FU_COUNT; i++) begin : fu_connections
+            for (j = 0; j < LANE_ISSUE_W; j++) begin : port_connections
+                assign fu_interfaces[i].in.ports[j].input_valid = lif.in.input_valid[j];
+                assign fu_interfaces[i].in.ports[j].v1 =          lif.in.v1[j];
+                assign fu_interfaces[i].in.ports[j].v2 =          lif.in.v2[j];
+                assign fu_interfaces[i].in.ports[j].usel =        lif.in.usel[j];
+                assign fu_interfaces[i].in.ports[j].vd =          lif.in.vd[j];
+                assign fu_interfaces[i].in.ports[j].rm =          lif.in.rm[j];
+                assign fu_interfaces[i].in.ports[j].mask =        lif.in.mask[j];
+                assign fu_interfaces[i].in.ports[j].alu_op =      lif.in.aluop[j];
             end
-            fu_interfaces[i].in.wb_ready =  lif.in.rc_ready[i]; //ready singals from writeback
-            lif.out.units[i] =              fu_interfaces[i].out;
-
+            
+            assign fu_interfaces[i].in.wb_ready = lif.in.ready[i];
+            assign lif.out.units[i] = fu_interfaces[i].out;
         end
-    end
+    endgenerate
     
 
 endmodule
