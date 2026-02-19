3ccaeeb7cb08de9ceaf3b60d0d8efc7b2e69968a
Joshua David Klug <klug1@asicfab.ecn.purdue.edu>
2026-02-18 20:19:49 -0500

    working execute unit 2

--- rtl/modules/scheduler/execution_units/bfD_sD_bfInt_intBF.sv
+++ rtl/modules/scheduler/execution_units/bfD_sD_bfInt_intBF.sv
@@ -11,7 +11,7 @@ module div_bf_scalar_convert_wrapper (
 typedef enum {start, latch, done} state;
 state n_state, cur_state;
 
-div_if divif
+div_if divif ();
 
 logic [31:0] bf_int_output, scalar_div_output, scalar_mod_output;
 logic [15:0] int_bf_output, bf_div_output;
@@ -23,34 +23,36 @@ logic latched_BF_Int, nlatched_BF_Int, cur_BF_Int;
 logic latched_Int_BF, nlatched_Int_BF, cur_Int_BF;
 logic counter_enable, counter_clear;
 logic [7:0] nlatchedRD, latchedRD;
-logic [3:0] latency, current_count;
-logic start_div_mod;
+logic [6:0] latency, current_count;
+logic start_div_mod, latch_valid;
+logic internal_temp;
 
 assign divif.in.operand1 = cur_input_1[15:0];
 assign divif.in.operand2 = cur_input_2[15:0];
-assign divif.in.valid_in = portmap.valid_in && cur_BFdiv;
+assign divif.in.valid_in = (portmap.valid_in || latch_valid) && cur_BFdiv;
+assign divif.in.ready_out = 1'b1;
 assign bf_div_output = divif.out.result;
 
-flex_counter counter (.clk(clk), .nrst(nRST), .enable(counter_enable), .clear(counter_clear), .count(current_count));
+flex_counter #(.N(7)) counter (.clk(clk), .nrst(nRST), .enable(counter_enable), .clear(counter_clear), .count(current_count));
 
-assign start_div_mod = (cur_Sdiv || cur_Smod) && portmap.valid_in;
+assign start_div_mod = (cur_Sdiv || cur_Smod) && (portmap.valid_in);
 
 div DIVIDER_BF16 (.CLK(clk), .nRST(nRST), .divif(divif));
 bf_to_int BF_INT_CONV (.inputBF(cur_input_1[15:0]), .outputInt(bf_int_output));
 int_to_bf INT_BF_CONV (.inputInt(cur_input_1), .outputBF(int_bf_output));
-socetlib_radix4_divider DIVIDER_SCALAR (.CLK(clk), .nRST(nRST), .start(start_div_mod), .is_signed(1), .dividend(cur_input_1), .divisor(cur_input_2), .quotient(scalar_div_output), .remainder(scalar_mod_output), .finished());
+socetlib_shift_test_restore_divider DIVIDER_SCALAR (.CLK(clk), .nRST(nRST), .start(start_div_mod), .is_signed(1'b1), .dividend(cur_input_1), .divisor(cur_input_2), .quotient(scalar_div_output), .remainder(scalar_mod_output), .finished(internal_temp));
 
 always_comb begin
     latency = 1;
     portmap.data_out = 32'b0;
     if(cur_BFdiv == 1) begin
-        latency = 15;
+        latency = 11;
         portmap.data_out = {16'b0, bf_div_output};
     end else if(cur_Sdiv == 1) begin
-        latency = 15;
+        latency = 66;
         portmap.data_out = scalar_div_output;
     end else if(cur_Smod == 1) begin
-        latency = 15;
+        latency = 66;
         portmap.data_out = scalar_mod_output;
     end else if(cur_BF_Int == 1) begin
         latency = 1;
@@ -63,19 +65,21 @@ end
 
 always_comb begin
     n_state = cur_state;
-    nlatched_bf1 = latched_bf1;
-    cur_bf_1 = portmap.bf1_in;
-    nlatched_bf2 = latched_bf2;
-    cur_bf_2 = portmap.bf2_in;
+    nlatched_input1 = latched_input1;
+    cur_input_1 = portmap.input1;
+    nlatched_input2 = latched_input2;
+    cur_input_2 = portmap.input2;
 
-    nlatched_add = latched_add;
-    cur_add = portmap.add;
-    nlatched_sub = latched_sub;
-    cur_sub = portmap.sub;
-    nlatched_mult = latched_mult;
-    cur_mult = portmap.mult;
-    nlatched_slt = latched_slt;
-    cur_slt = portmap.slt;
+    nlatched_BFdiv = latched_BFdiv;
+    cur_BFdiv = portmap.bf_div;
+    nlatched_Sdiv = latched_Sdiv;
+    cur_Sdiv = portmap.s_div;
+    nlatched_Smod = latched_Smod;
+    cur_Smod = portmap.s_mod;
+    nlatched_BF_Int = latched_BF_Int;
+    cur_BF_Int = portmap.BF_to_int;
+    nlatched_Int_BF = latched_Int_BF;
+    cur_Int_BF = portmap.int_to_BF;
 
     portmap.ready_in = 1'b1;
     portmap.valid_out = 1'b0;
@@ -85,6 +89,7 @@ always_comb begin
 
     counter_enable = 1'b0;
     counter_clear = 1'b0;
+    latch_valid = 1'b0;
 
     case (cur_state)
         start: begin
@@ -94,13 +99,14 @@ always_comb begin
                     n_state = done;
                 end
             end
-            nlatched_bf1 = portmap.bf1_in;
-            nlatched_bf2 = portmap.bf2_in;
+            nlatched_input1 = portmap.input1;
+            nlatched_input2 = portmap.input2;
 
-            nlatched_add = portmap.add;
-            nlatched_sub = portmap.sub;
-            nlatched_mult = portmap.mult;
-            nlatched_slt = portmap.slt;
+            nlatched_BFdiv = portmap.bf_div;
+            nlatched_Sdiv = portmap.s_div;
+            nlatched_Smod = portmap.s_mod;
+            nlatched_BF_Int = portmap.BF_to_int;
+            nlatched_Int_BF = portmap.int_to_BF;
 
             nlatchedRD = portmap.rdIn;
         end
@@ -113,15 +119,17 @@ always_comb begin
             portmap.ready_in = 1'b0;
             portmap.valid_out = 1'b0;
 
-            cur_bf_1 = latched_bf1;
-            cur_bf_2 = latched_bf2;
+            cur_input_1 = latched_input1;
+            cur_input_2 = latched_input2;
 
-            cur_add = latched_add;
-            cur_sub = latched_sub;
-            cur_mult = latched_mult;
-            cur_slt = latched_slt;
+            cur_BFdiv = latched_BFdiv;
+            cur_Sdiv = latched_Sdiv;
+            cur_Smod = latched_Smod;
+            cur_BF_Int = latched_BF_Int;
+            cur_Int_BF = latched_Int_BF;
 
             portmap.rdOut = latchedRD;
+            latch_valid = 1'b1;
         end
         done: begin
             if(portmap.ready_out) begin
@@ -132,15 +140,17 @@ always_comb begin
             portmap.ready_in = 1'b0;
             portmap.valid_out = 1'b1;
 
-            cur_bf_1 = latched_bf1;
-            cur_bf_2 = latched_bf2;
+            cur_input_1 = latched_input1;
+            cur_input_2 = latched_input2;
 
-            cur_add = latched_add;
-            cur_sub = latched_sub;
-            cur_mult = latched_mult;
-            cur_slt = latched_slt;
+            cur_BFdiv = latched_BFdiv;
+            cur_Sdiv = latched_Sdiv;
+            cur_Smod = latched_Smod;
+            cur_BF_Int = latched_BF_Int;
+            cur_Int_BF = latched_Int_BF;
 
             portmap.rdOut = latchedRD;
+            latch_valid = 1'b0;
         end
     endcase
 end
