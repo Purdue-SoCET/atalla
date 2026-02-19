cd05f773542898225e5f85309231c6eecda8e29f
jasonlyst <jlyst@purdue.edu>
2026-02-18 20:31:59 -0500

    added tccdl and tccds and tfaw timing parameter properties. I need to work on tfaw a little more because this is kind of a weird property, will work on this later, maybe Eddie and I or someone else can figure it out. I am not quite sure how to deal with counting the number of accesses within a function, but maybe it is not that hard.

--- src/testbench/nb_barb_prop.sv
+++ src/testbench/nb_barb_prop.sv
@@ -5,21 +5,40 @@
 module nb_barb_prop(
     input logic CLK,
     input logic nRST,
-    // 2/13 - TODO: ADD SIGNALS
+    // 2/13 - TODO: ADD SIGNALS, DONT ADD SIGNALS, ADD PROPERTIES
     ddr_controller_if.backend_arb barb
 );
     import dram_pkg::*;
     import cpu_types_pkg::*;
 
+    function automatic integer encode (input logic [15:0] in);
+        for (int i = 0; i < 16; i++) begin
+            if (in[i]) return i;
+        end
+        return -1;
+    endfunction
+
     property onehot
-        @(posedge CLK) disable iff (!nRST || (barb.be_cmd[BANK_NUM-1:0]) == ((BANK_NUM){REFRESH}) )
-        $onehot0(barb.be_arb);
+        @(posedge CLK) disable iff (!nRST)
+        $onehot0(barb.be_arb) || (barb.be_cmd[BANK_NUM-1:0]) == ((BANK_NUM){REFRESH});
     endproperty
 
     property timing_tCCD_L
+        integer bg_last; 
         @(posedge CLK) disable iff (!nRST)
-        ($onehot(be_queue_ready & be_arb) ) |-> [*tCCD_L] ~|(barb.be_arb)
+        ( $onehot(barb.be_queue_ready & barb.be_arb), bg_last = encode(barb.be_queue_ready & barb.be_arb) ) |=> [*tCCD_L] (~|(barb.be_arb) || encode(barb.be_queue_ready & barb.be_arb) != bg_last);
     endproperty 
 
+    property timing_tCCD_S
+        @(posedge CLK) disable iff(!nRST)
+        $onehot(be_queue_ready & be_arb) |=> [*tCCD_S] ~|(barb.be_arb);
+    endproperty
+
+    property timing_tFAW
+        @(posedge CLK) disable iff(!nRST)
+        
+
+    endproperty
 
 endmodule 
+
