5f2fa85f4fd9b97d6bce0f45b3fe2b58c2f2b957
Nicha Muninnimit <nmuninni@purdue.edu>
2026-02-19 16:04:36 -0500

    col starts fix

--- rtl/modules/systolic_array/mul_grid.sv
+++ rtl/modules/systolic_array/mul_grid.sv
@@ -39,7 +39,7 @@ module mul_grid (
         if (!nRST) begin
             col_starts <= '0;
         end else if (!mul_stall) begin
-            col_starts <= {col_starts[N-1:1], act_en};
+            col_starts <= {col_starts[N-2:0], act_en};
         end
     end
 
