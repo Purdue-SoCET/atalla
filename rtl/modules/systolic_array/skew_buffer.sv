f1de848d15f817978ec7ef47b1ce96fd401e4d7a
Seongjoong Yim <yim13@asicfab.ecn.purdue.edu>
2026-02-20 22:18:16 -0500

    removed 1 unnecesary sram depth

--- rtl/modules/systolic_array/skew_buffer.sv
+++ rtl/modules/systolic_array/skew_buffer.sv
@@ -13,7 +13,7 @@ module skew_buffer #(
     localparam int READ_LATENCY      = 1,    // sram read latency
     localparam int WRITE_LATENCY     = 1,    // sram write latency (MUST be equal to read latency)
     // since first column gets 0 delay, last column can get NUM_COLS - 1 delay coefficient
-    localparam int SRAM_DEPTH = RECT_DELAY + (DELAY_SLOPE * (NUM_COLS - 1)) + 2,
+    localparam int SRAM_DEPTH = RECT_DELAY + (DELAY_SLOPE * (NUM_COLS - 1)) + 1,
     localparam int PTR_WIDTH = $clog2(SRAM_DEPTH)
 ) (
     input  logic clk, n_rst,
