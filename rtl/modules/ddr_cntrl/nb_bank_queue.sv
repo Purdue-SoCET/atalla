7737f8ff88607bbdf781401b8bf40452d7a4af65
Eddie Hu <hu927@purdue.edu>
2026-02-23 19:29:20 -0500

    Initial bq updates

--- rtl/modules/ddr_cntrl/nb_bank_queue.sv
+++ rtl/modules/ddr_cntrl/nb_bank_queue.sv
@@ -9,17 +9,29 @@
     logic fe_write_bq;
     logic [$clog2(BANK_NUM)-1:0] fe_full; // [QUEUE_SIZE-1:0]
 */
-
-typedef struct packed {
-    logic [ROW_BITS-1:0] row;
-    logic [COLUMN_BITS-1:0] column;
-    logic write;
-    logic [$clog2(ID_NUM)-1:0] id_addr;
-} bq_slot_t;
-
 module nb_bank_queue(
     input logic CLK, nRST,
     ddr_controller_if.bq bqif
 );
+
+    typedef struct packed {
+        logic [ROW_BITS-1:0] row;
+        logic [COLUMN_BITS-1:0] column;
+        logic write;
+        logic [$clog2(ID_NUM)-1:0] id_addr;
+    } bq_slot_t;
+
+    bq_slot_t bq_slot;
+    assign bq_slot = {bqif.fe_r, bqif.fe_c, bqif.fe_write, bqif.fe_id};
+
+    sync_fifo #(.DEPTH(BANK_NUM), .DWIDTH($bits(bq_slot_t))) bq_fifo (
+        .clk(CLK), .rstn(nRST),
+        .wr_en(bqif.fe_write_bq),
+        .din(),
+        .rd_en(~bqif.fe_write_bq),
+        .dout({bqif.bq_r, bqif.bq_c, bqif.fe}),
+        .full(bqif.fe_full),
+        .empty(bqif.fe_ready)
+    );
 
 endmodule
