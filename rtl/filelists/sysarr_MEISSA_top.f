9f7ad0c3ca3d1806e952dcb87a0fe3cfe5411a32
Seongjoong Yim <yim13@asicfab.ecn.purdue.edu>
2026-03-04 15:10:04 -0500

    update for TPU

--- rtl/filelists/sysarr_MEISSA_top.f
+++ rtl/filelists/sysarr_MEISSA_top.f
@@ -18,13 +18,21 @@
 ./rtl/modules/common/general/wallacetree_11b_2c.sv
 ./rtl/modules/systolic_array/arithmetic/mul_fp16.sv
 ./rtl/modules/systolic_array/arithmetic/add_fp16.sv
+./rtl/modules/systolic_array/arithmetic/add_fp16_4_input.sv
 ./rtl/modules/common/memory/sram_bank.sv
 
 // High-level modules
 ./rtl/modules/systolic_array/mul_grid.sv
 ./rtl/modules/systolic_array/pipelined_adder_tree.sv
 ./rtl/modules/systolic_array/skew_buffer.sv
+// TPU
+./rtl/modules/systolic_array/TPU/TPU_buffer.sv
+./rtl/modules/systolic_array/TPU/TPU_control_unit.sv
+./rtl/modules/systolic_array/TPU/TPU_MAC_4_input.sv
+./rtl/modules/systolic_array/TPU/TPU_mul_cell.sv
+
 
 // Top
 ./rtl/modules/systolic_array/sysarr_MEISSA_top.sv
+./rtl/modules/systolic_array/TPU/TPU_top.sv
 ./tb/unit/systolic_array/systolic_array_tb_pkg.sv
