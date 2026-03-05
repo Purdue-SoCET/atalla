7c3365985ef3e671d7ab6828d6feba8e33f8bc02
Nicha Muninnimit <nmuninni@purdue.edu>
2026-03-05 16:21:58 -0500

    option for MEISSA mixed adder tree vs 2input

--- tb/unit/systolic_array/sysarr_MEISSA_top_tb.sv
+++ tb/unit/systolic_array/sysarr_MEISSA_top_tb.sv
@@ -233,6 +233,10 @@ module sysarr_MEISSA_top_tb();
 
 generate
     if(VERSION == "MEISSA") begin
+        // DUT Instantiation: MIXED ADDER TREE
+        // sysarr_MEISSA_top #(.USE_MIXED_ADDER(1)) DUT (CLK, nRST, gsau_if);
+
+        // DUT Instantiation: 2-INPUT ADDER TREE
         sysarr_MEISSA_top DUT (CLK, nRST, gsau_if);
     end
     else if (VERSION == "TPU") begin
