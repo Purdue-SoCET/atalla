f7b79a04dc53b3c5a0f74c6bb4c1faf76a49056e
jasonlyst <jlyst@purdue.edu>
2026-03-07 15:13:43 -0500

    moved files to fit new repo structure

---
+++ tb/unit/ddr_cntrl/testbench/nb_wdata_queue_fifo_tb.sv
@@ -0,0 +1,16 @@
+`include "ddr_controller_if.vh"
+`timescale 1 ns / 1 ps
+
+module nb_wdata_queue_fifo_tb;
+    // parameters
+    logic CLK;
+
+
+    // interfaces/modports
+
+
+    // instantiations
+
+
+
+endmodule
