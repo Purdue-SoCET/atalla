ebaa38c8f49f292ff03152f80ff91624cacbd372
jasonlyst <jlyst@purdue.edu>
2026-03-07 15:13:43 -0500

    moved files to fit new repo structure

---
+++ tb/unit/ddr_cntrl/testbench/nb_wdata_queue_prop.sv
@@ -0,0 +1,37 @@
+`include "dram_pkg.vh"
+`include "cpu_types_pkg.vh"
+`include "ddr_controller_if.vh"
+
+module nb_wdata_queue_prop(
+    input logic CLK,
+    input logic nRST,
+    // 2/13 - TODO: ADD SIGNALS
+    ddr_controller_if.wdata_queue wdq
+);
+    import dram_pkg::*;
+    import cpu_types_pkg::*;
+
+    // // PROPERTIES
+    // 2/13 - TODO: Figure out proper syntax
+
+    property AXI_wdata_handshake;
+        @(posedge CLK) disable iff (!nRST)
+        (wdq.bwvalid && wdq.bwready) |=> !wdq.bwvalid;
+    endproperty
+
+    property AXI_wdata_wait;
+        @(posedge CLK) disable iff (!nRST)
+        (wdq.bwvalid && !wdq.bwready) |=>  $stable(wdq.bwvalid); 
+    endproperty
+
+    property AXI_wdata_handshake_cycle;
+        @(posedge CLK) disable iff (!nRST)
+        (wdq.bwvalid && wdq.bwready) |-> wdq.bwvalid; 
+    endproperty
+
+    sequence 
+
+    endsequence
+        
+endmodule 
+
