10526ec72ecc4c6524f39b372df7ba4e5428579f
jasonlyst <jlyst@purdue.edu>
2026-03-07 16:22:12 -0500

    modified property files to fit timing change for wdata queue and backend arbiter

--- tb/unit/ddr_cntrl/testbench/nb_wdata_queue_prop.sv
+++ tb/unit/ddr_cntrl/testbench/nb_wdata_queue_prop.sv
@@ -29,6 +29,11 @@ module nb_wdata_queue_prop(
         (wdq.bwvalid && wdq.bwready) |-> wdq.bwvalid; 
     endproperty
 
+    property AXI_wdata_tCWL;
+        @(posedge CLK) disable iff (!nRST)
+        (wdq.be_write && (wdq.be_id == Q_ID) ) |-> [*(tCWL-1)](!wdq.ddr_we) ##1 $rose(wdq.ddr_we) ##1 $fell(wdq.ddr_we);
+    endproperty
+
     sequence 
 
     endsequence
