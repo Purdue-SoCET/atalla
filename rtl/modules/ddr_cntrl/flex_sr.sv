b21b1c327a18fd8826998e5fe2ff7ef373892423
jasonlyst <jlyst@purdue.edu>
2026-03-08 20:19:23 -0400

    finished backend arbiter design, updated properties to include verification of wdata queue and backend arbiter working together

---
+++ rtl/modules/ddr_cntrl/flex_sr.sv
@@ -0,0 +1,37 @@
+`timescale 1ns / 10ps
+
+module flex_sr #(
+                SIZE = 8,
+                MSB_FIRST = 0,
+                RING = 0
+                ) (
+                input logic CLK,
+                input logic nRST,
+                input logic shift_enable,
+                input logic load_enable,
+                input logic serial_in,
+                input logic [SIZE - 1:0] parallel_in,
+                //output logic serial_out,
+                output logic [SIZE - 1:0] parallel_out
+                );
+
+    logic [SIZE - 1: 0] sr; //intermediate shift register. 
+    logic serial_out;
+    logic serial_in_feed;
+    assign serial_in_feed = RING ? serial_out : serial_in;
+
+    always_ff @(posedge clk, negedge n_rst) begin
+        if(!n_rst)
+            sr <= RING ? 'b1 : 'b0;
+        else  
+            sr <= load_enable  ? parallel_in : (shift_enable ? (MSB_FIRST ? {sr[SIZE - 2:0], serial_in_feed} : {serial_in_feed, sr[SIZE - 1:1]}) : sr);
+    end
+
+    
+
+    assign serial_out = MSB_FIRST ? sr[SIZE - 1] : sr[0];
+    assign parallel_out = sr;
+            
+
+
+endmodule
