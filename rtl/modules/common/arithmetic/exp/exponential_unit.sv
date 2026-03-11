8136bb016ad144ea9cf470c293735f56ded80bbf
adamg345 <144060096+adamg345@users.noreply.github.com>
2026-03-11 17:42:51 -0400

    Add files via upload
    Uploading buggy exponential with multiplier. 
    
    Signed-off-by: adamg345 <144060096+adamg345@users.noreply.github.com>

--- rtl/modules/common/arithmetic/exp/exponential_unit.sv
+++ rtl/modules/common/arithmetic/exp/exponential_unit.sv
@@ -1,36 +1,43 @@
 `include "vector_pkg.vh"
 `include "exp_if.vh"
 
-import vector_pkg::*; 
 
 module exponential_unit (
     input  logic clk, 
     input  logic rst_n,
-    exp_if.exif  ex  // The interface port
+    exp_if.exif  expif  // The interface port
 );
+    import vector_pkg::*; 
 
     // --- Constants ---
     localparam signed [15:0] LOG2_E_Q12 = 16'd5909;
     localparam signed [31:0] ROUND_BIT   = 32'sh1000; 
     localparam logic [14:0] OVERFLOW_MAG  = 15'h42B1; 
     localparam logic [14:0] UNDERFLOW_MAG = 15'h42B8; 
+    localparam logic [14:0] NAN_MAG = 15'h7f81;
 
-    bf16_t x_in;
-    assign x_in = bf16_t'(ex.in.operand);
+    logic [15:0] x_in;
+    logic sign;
+    logic [7:0] exp;
+    logic [6:0] frac;
+
+    assign x_in = expif.in.operand;
+    assign x_in_sign = x_in[15];
+    assign x_in_exp = x_in[14:7];
+    assign x_in_frac = x_in[6:0];
 
     logic pipe_en;
-    
 
-    assign pipe_en = ex.in.ready_out || !ex.out.valid_out;
+    assign pipe_en = expif.in.ready_out || !expif.out.valid_out;
 
-    assign ex.out.ready_in = pipe_en;
+    assign expif.out.ready_in = pipe_en;
 
     logic [31:0] Q8_signed_st2;
-    logic        overflow_st2, underflow_st2, valid_st2;
+    logic        is_NAN_st2, overflow_st2, underflow_st2, valid_st2;
 
     logic signed [8:0] z_int_st3;
     logic [6:0]        lut_mantissa_st3;
-    logic              overflow_st3, underflow_st3, valid_st3;
+    logic              is_NAN_st3, overflow_st3, underflow_st3, valid_st3;
 
     // --- Internal Signals ---
     logic signed [9:0]  shift_val;
@@ -40,46 +47,57 @@ module exponential_unit (
     logic signed [31:0] z_full;    
     logic signed [8:0]  z_int;     
     logic [6:0]         z_frac;  
-    logic               is_overflow_comb, is_underflow_comb;
+    logic               is_NAN_comb, is_overflow_comb, is_underflow_comb;
+
+    logic mult_done;
+    logic reduce_partial_prods;
+    logic signed [63:0] mult_out;
 
     // =========================================================
     // STAGE 1: Input Logic
     // =========================================================
     
-    assign is_overflow_comb  = (x_in.sign == 1'b0 && x_in[14:0] > OVERFLOW_MAG);
-    assign is_underflow_comb = (x_in.sign == 1'b1 && x_in[14:0] > UNDERFLOW_MAG);
-    assign shift_val         = $signed({2'b0, x_in.exp}) - 10'd127; 
+    assign is_NAN_comb = (x_in[14:0] >= NAN_MAG);
+    assign is_overflow_comb  = (x_in_sign == 1'b0 && x_in[14:0] > OVERFLOW_MAG);
+    assign is_underflow_comb = (x_in_sign == 1'b1 && x_in[14:0] > UNDERFLOW_MAG);
+    assign shift_val         = $signed({2'b0, x_in_exp}) - 10'd127; 
 
     always_comb begin
         Q8_base = '0;
         if (shift_val >= 0) 
-            Q8_base = {24'b0, 1'b1, x_in.frac} << (shift_val + 1);
+            Q8_base = {24'b0, 1'b1, x_in_frac} << (shift_val + 1);
         else 
-            Q8_base = {24'b0, 1'b1, x_in.frac} >> (-shift_val - 1);
+            Q8_base = {24'b0, 1'b1, x_in_frac} >> (-shift_val - 1);
         
-        Q8_signed = x_in.sign ? (-$signed(Q8_base)) : $signed(Q8_base);
+        Q8_signed = x_in_sign ? (-$signed(Q8_base)) : $signed(Q8_base);
     end
 
     // Stage 1 -> 2 Registers
     always_ff @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
             Q8_signed_st2 <= '0;
+            is_NAN_st2 <= 1'b0;
             overflow_st2  <= 1'b0;
             underflow_st2 <= 1'b0;
             valid_st2     <= 1'b0;
         end else if (pipe_en) begin
             Q8_signed_st2 <= Q8_signed;
+            is_NAN_st2 <= is_NAN_comb;
             overflow_st2  <= is_overflow_comb;
             underflow_st2 <= is_underflow_comb;
-            valid_st2 <= ex.in.valid_in; 
+            valid_st2 <= expif.in.valid_in; 
         end
     end
 
     // =========================================================
     // STAGE 2: Mult and LUT
     // =========================================================
+    
+    // USES 32-BIT MULTIPLIER FROM DIGITAL DESIGN TEAM.
+    wt_mult mult32(.CLK(clk), .nRST(rst_n), .a(Q8_signed_st2), .b({16'b0,LOG2_E_Q12}), .ready(valid_st2), .done(mult_one), .out(mult_out));
+    assign z_full = mult_out[31:0] + ROUND_BIT;
 
-    assign z_full = (Q8_signed_st2  * LOG2_E_Q12) + ROUND_BIT;
+    //assign z_full = (Q8_signed_st2 * LOG2_E_Q12) + ROUND_BIT;
     assign z_int  = z_full[28:20];
     assign z_frac = z_full[19:13]; 
 
@@ -93,12 +111,14 @@ module exponential_unit (
         if(!rst_n) begin
             z_int_st3        <= '0;
             lut_mantissa_st3 <= '0;
+            is_NAN_st3 <= 1'b0;
             overflow_st3     <= 1'b0;
             underflow_st3    <= 1'b0;
             valid_st3        <= 1'b0;
         end else if (pipe_en) begin
             z_int_st3        <= z_int;
             lut_mantissa_st3 <= lut_mantissa;
+            is_NAN_st3 <= is_NAN_st2;
             overflow_st3     <= overflow_st2;
             underflow_st3    <= underflow_st2;
             valid_st3        <= valid_st2;
@@ -114,18 +134,21 @@ module exponential_unit (
 
     // Drive Output Signals
     always_comb begin
-        ex.out.result = '0;
-        if (overflow_st3) 
-            ex.out.result = 16'h7F80; 
-        else if (underflow_st3) 
-            ex.out.result = 16'h0000;
-        else begin
-            if (final_exp_calc >= 255)      ex.out.result = 16'h7F80;
-            else if (final_exp_calc <= 0)   ex.out.result = 16'h0000;
-            else                            ex.out.result = {1'b0, final_exp_calc[7:0], lut_mantissa_st3};
+        expif.out.result = '0;
+        if (is_NAN_st3) begin
+            expif.out.result = 16'h7fc0;
+        end else if (overflow_st3) begin
+            expif.out.result = 16'h7F80;
+        end else if (underflow_st3) begin
+            expif.out.result = 16'h0000;
+        end else begin
+            if (final_exp_calc >= 255)      expif.out.result = 16'h7F80;
+            else if (final_exp_calc <= 0)   expif.out.result = 16'h0000;
+            else
+                expif.out.result = {1'b0, final_exp_calc[7:0], lut_mantissa_st3};
         end
     end
     
-    assign ex.out.valid_out = valid_st3;
+    assign expif.out.valid_out = valid_st3;
 
 endmodule
---
+++ rtl/modules/common/arithmetic/exp/wt_mult.sv
@@ -0,0 +1,109 @@
+`include "wt_mult_pkg.sv"
+
+import wt_mult_pkg::*;
+
+/// Fully pipelined wallace tree multiplier
+/// Three stages:
+///     1) Generate partial products (1 stage)
+///     2) Reduce partial products in a tree using half and full adders (lgN stages)
+///     3) Reduce final two outputs (1 stage)
+module wt_mult#(
+    parameter int WIDTH = 32,
+    parameter logic MERGE_PROD_GEN_REDUCE_STAGES=1,
+    parameter int STAGE_DEPTH=8,
+    parameter logic MERGE_REDUCE_FINAL_STAGES=0,
+    // Generate partial products using Baugh-Wooley method
+    parameter int SIGNED=0
+)(
+    input logic CLK, nRST,
+    input logic ready,
+    input logic [WIDTH-1:0] a, b,
+    output logic done,
+    output logic [WIDTH*2-1:0] out,
+    output logic [WIDTH*2-1:0] [WIDTH-1:0] reduce_partial_prods
+);
+    logic [WIDTH*2-1:0] [WIDTH-1:0] partial_prods;
+    logic reduce_ready, reduce_done;
+    logic [WIDTH*2-1:0] out_a, out_b;
+    logic [WIDTH*2-1:0] int_out;
+
+    // Generate a sequence of [1, 2, 3, ..., WIDTH, WIDTH-1, WIDTH-2, ..., 1, 0]
+    function automatic logic [WIDTH*2-1:0] [31:0] init_depth();
+        logic [WIDTH*2-1 : 0] [31 : 0] result;
+        for (int i = 1; i <= WIDTH*2; i++) begin
+            if (i < WIDTH) begin
+                result[i - 1] = i;
+            end else begin
+                result[i - 1] = 2 * WIDTH - i;
+            end
+            if (SIGNED) begin
+                result[WIDTH] = WIDTH;
+            end
+        end
+        return result;
+    endfunction
+
+    // Stage 1: Generate partial products
+    wt_partial_prod_gen #(
+        .WIDTH(WIDTH),
+        .SIGNED(SIGNED)
+    ) partial_prod_gen (
+        .a(a),
+        .b(b),
+        .partial_prods(partial_prods)
+    );
+
+    generate
+        if (MERGE_PROD_GEN_REDUCE_STAGES) begin
+            assign reduce_ready = ready;
+            assign reduce_partial_prods = partial_prods;
+        end else begin
+            always_ff @(posedge CLK, negedge nRST) begin
+                if (!nRST) begin
+                    reduce_ready <= 0;
+                    reduce_partial_prods <= '0;
+                end else begin
+                    reduce_ready <= ready;
+                    reduce_partial_prods <= partial_prods;
+                end
+            end
+        end
+    endgenerate
+
+    // Stage 2: Reduce partial products
+    localparam logic [WIDTH*2-1:0] [31:0] DEPTH = init_depth();
+
+    wt_reduction #(
+        .WIDTH(WIDTH),
+        .STAGE_DEPTH(STAGE_DEPTH),
+        .CURR_STAGE_DEPTH(STAGE_DEPTH - !MERGE_PROD_GEN_REDUCE_STAGES),
+        .DEPTH(DEPTH)
+    ) reduce (
+        .CLK(CLK),
+        .nRST(nRST),
+        .ready(reduce_ready),
+        .partial_prods(reduce_partial_prods),
+        .done(reduce_done),
+        .out_a(out_a),
+        .out_b(out_b)
+    );
+
+    // Stage 3: Add remaining two values
+    assign int_out = out_a + out_b;
+    generate
+        if (MERGE_REDUCE_FINAL_STAGES) begin
+            assign out = SIGNED ? {!int_out[WIDTH*2-1], int_out[WIDTH*2-2:0]} : int_out;
+            assign done = reduce_done;
+        end else begin
+            always_ff @(posedge CLK, negedge nRST) begin
+                if (!nRST) begin
+                    out <= 0;
+                    done <= 0;
+                end else begin
+                    out <= SIGNED ? {!int_out[WIDTH*2-1], int_out[WIDTH*2-2:0]} : int_out;
+                    done <= reduce_done;
+                end
+            end
+        end
+    endgenerate
+endmodule
