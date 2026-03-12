e51f4304b38b664f0ea8869bedd48b195485e79d
Mary Francis <franci92@asicfab.ecn.purdue.edu>
2026-03-12 16:55:50 -0400

    works w layernorm

--- emulator/run.py
+++ emulator/run.py
@@ -1,11 +1,17 @@
 import argparse
 
-from .src.emulator import run 
-from .src.misc.memory import Memory
-from .src.components.scalar_register_file import ScalarRegisterFile
-from .src.components.vector_register_file import VectorRegisterFile
-from .src.components.execute import ExecuteUnit
-from .src.components.scpad import Scratchpad
+# from .src.emulator import run 
+# from .src.misc.memory import Memory
+# from .src.components.scalar_register_file import ScalarRegisterFile
+# from .src.components.vector_register_file import VectorRegisterFile
+# from .src.components.execute import ExecuteUnit
+# from .src.components.scpad import Scratchpad
+from src.emulator import run 
+from src.misc.memory import Memory
+from src.components.scalar_register_file import ScalarRegisterFile
+from src.components.vector_register_file import VectorRegisterFile
+from src.components.execute import ExecuteUnit
+from src.components.scpad import Scratchpad
 
 if __name__ == "__main__":
 
