3e80ddb81c585da06b5c4d30e9a35d6dca242fb5
jasonlyst <jlyst@purdue.edu>
2026-03-26 19:17:35 -0400

    updated another line in the script.

--- scripts/ddr_cntrl/nb_wdata_wrapper.tcl
+++ scripts/ddr_cntrl/nb_wdata_wrapper.tcl
@@ -49,7 +49,8 @@ vmap work work
 
 # --- Compilation ---
 # -mfcu (Multi-File Compilation Unit) is good here to resolve cross-file dependencies
-vlog -sv -mfcu {*}$INC_FLAGS {*}$SRC_FILES
+vlog -sv -compile_uselibs -cover bst -sv -pedanticerrors -lint -mfcu {*}$INC_FLAGS {*}$SRC_FILES
+
 
 puts "=============================================================="
 puts "Compilation complete. Launching simulation for $TB_TOP"
