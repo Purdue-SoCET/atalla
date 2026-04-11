f3cd262597d7caa4c7d41eae936e239aa4c29cf5
Akhil Yada <yadaa@purdue.edu>
2026-04-11 18:17:47 -0400

    done transpose unit (hopefully)

--- rtl/modules/vector/transpose_unit.sv
+++ rtl/modules/vector/transpose_unit.sv
@@ -4,24 +4,9 @@
 module transpose_unit (
     input logic CLK, nRST,
     transpose_unit_if.transpose tif
-);
+);  
 
-    xbar_if #(.SIZE(tif.VEC_LEN), .DWIDTH(tif.DATA_W)) xif();
-
-    group_t input_matrix [SIZE];
-
-    // Input CLOS Network (Barrel Shifter)
-    assign xif.clk = CLK;
-    assign xif.n_rst = nRST;
-    assign xif.en = tif.valid_in;
-    always_comb begin : assign_vector
-    for (int i = 0; i < tif.VEC_LEN; i++) begin
-        xif.in[i].din   = tif.vec_in[i];
-        xif.in[i].shift = '0; // Or whatever logic you determine later TODO: FIX THIS
-        end
-    end
-
-    clos #(.CLOS_SIZE(tif.VEC_LEN)) clos_inst (.xif(xif));
+    xbar_if #(.SIZE(tif.VEC_LEN), .DWIDTH(tif.DATA_W)) xif(.clk(CLK), .n_rst(nRST));
 
     typedef enum logic [1:0] {
         IDLE, 
@@ -31,35 +16,15 @@ module transpose_unit (
 
     state_t state, n_state;
     logic [4:0] count, n_count; 
+    logic ren;
+    logic wen;
+
+    // Buses to collect data from all SRAM banks
+    logic [tif.DATA_W-1:0] sram_rdata [tif.VEC_LEN-1:0];
+    logic [tif.VEC_LEN-1:0] sram_rdone;
+
 
     // --- Control FSM ---
-    always_comb begin : next_state
-        n_state = state;
-        n_count = count;
-        unique case(state)
-            IDLE: begin 
-                if(tif.in.valid_in && tif.in.push_req) begin 
-                    n_state = PUSHING;
-                    n_count = count + 1; 
-                end
-            end
-            PUSHING: begin
-                if(tif.out.ready_out && tif.in.pop_req) begin 
-                    n_state = POPPING;
-                    n_count = 30; // TODO: Decide if this is right
-                end
-                if(tif.in.valid_in && tif.in.push_req) n_count = count + 1;
-            end
-            POPPING: begin
-                if(count == 0) begin 
-                    n_state = IDLE;
-                    n_count = 0;
-                end
-                if(tif.out.ready_out && tif.in.pop_req) n_count = count - 1;
-            end
-        endcase
-    end
-
     always_ff @(posedge CLK or negedge nRST) begin : general_ff
         if (!nRST) begin
             state <= IDLE;
@@ -70,10 +35,56 @@ module transpose_unit (
         end
     end
 
-    assign tif.valid_out  = (state == PUSHING);
-    assign tif.ready_out  = (state == POPPING);
+    always_comb begin : next_state
+        n_state = state;
+        n_count = count;
+        unique case(state)
+            IDLE: begin 
+                if(tif.valid_in && tif.push_req) begin 
+                    n_state = PUSHING;
+                    n_count = 0; 
+                end
+            end
+            PUSHING: begin
+                if(count == 31 && tif.push_req) begin
+                    n_state = POPPING;
+                    n_count = 0;
+                end else if(tif.valid_in && tif.push_req) begin
+                    n_count = count + 1;
+                end
+            end
+            POPPING: begin
+                if(count == 31 && tif.pop_req) begin 
+                    n_state = IDLE;
+                    n_count = 0;
+                end else if(tif.ready_out && tif.pop_req) begin
+                    n_count = count + 1;
+                end
+            end
+        endcase
+    end
 
-    // --- AGU and Bank Logic ---
+    // --- Shared Clos Network Logic ---
+    assign xif.en = (state == PUSHING && tif.valid_in) || (state == POPPING && tif.pop_req);
+
+    always_comb begin : clos_input_mux
+        for (int i = 0; i < tif.VEC_LEN; i++) begin
+            if (state == PUSHING) begin
+                xif.in[i].din = tif.vec_in[i];
+                xif.in[i].shift = (i + count) % tif.VEC_LEN;
+            end else begin
+                xif.in[i].din = sram_rdata[i];
+                xif.in[i].shift = (i + (tif.VEC_LEN - count)) % tif.VEC_LEN;
+            end
+        end
+    end
+
+    clos #(.CLOS_SIZE(tif.VEC_LEN), .CLOS_DWIDTH(tif.DATA_W)) clos_inst (.xif(xif));
+
+    assign ren = state == POPPING;
+    assign wen = tif.in.valid_in && state == PUSHING;
+
+    // --- SRAM Bank Instantiation ---
     genvar b;
     integer i;
     generate
@@ -82,40 +93,36 @@ module transpose_unit (
             logic [tif.DATA_W-1:0] rdata, wdata;
             logic rdone;
 
-            // assign bank_addr = (state == POPPING) ? 
-            //                    (b + tif.VEC_LEN - count) % tif.VEC_LEN : 
-            //                    count;
+            assign bank_raddr = (b + (tif.VEC_LEN - count)) % tif.VEC_LEN;
+
             sram_bank #(
                 .WIDTH(tif.DATA_W), 
-                .HEIGHT(32)
+                .HEIGHT(tif.VEC_LEN)
             ) bank_inst (
                 .clk(CLK), 
                 .n_rst(nRST),
-                .ren(state == POPPING),
-                .raddr(bank_addr),
-                .rdata(rdata),
-                .rdone(rdone),
-                .wen(state == PUSHING),
-                .waddr(bank_addr),
-                .wdata(wdata),
+                .ren(ren),
+                .raddr(bank_raddr),
+                .rdata(sram_rdata[b]),
+                .rdone(sram_rdone[b]),
+                .wen(wen),
+                .waddr(count),
+                .wdata(xif.out[b]),
                 .busy(),
                 .wdone()
             );
-
-            for(i = 0; i < tif.VEC_LEN; i++) begin
-                assign bank_addr = i;
-                assign wdata = xif.out[i];
-            end
-
-            assign xif.in[b].din   = rdata;
-            assign xif.in[b].shift = (b + tif.VEC_LEN - count) % tif.VEC_LEN;
-            
-            // Only need to track rdone from one bank if they are synchronized
-            if (b == 0) assign tif.vec_out_valid = (state == POPPING) && rdone;
         end
-        
     endgenerate
 
-    assign tif.vec_out = {>>{xif.out}};
+    // --- Output Assignments ---
+    assign tif.valid_out = (state == POPPING) && sram_rdone[0];
+    assign tif.ready_in = (state == IDLE || state == PUSHING);
+    
+    // The final output is the output of the Clos network during POPPING
+    always_comb begin
+        for(int i = 0; i < 32; i++) begin
+            tif.vec_out[i] = (state == POPPING) ? xif.out[i] : '0;
+        end
+    end
 
 endmodule
