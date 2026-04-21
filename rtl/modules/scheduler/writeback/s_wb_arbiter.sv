859b6e4dbcec58eea213d57fe7df86d53e08c3b0
micha3ll3e <lee4279@purdue.edu>
2026-04-21 14:03:06 -0400

    Comb loop?

--- rtl/modules/scheduler/writeback/s_wb_arbiter.sv
+++ rtl/modules/scheduler/writeback/s_wb_arbiter.sv
@@ -5,19 +5,23 @@
 // SCALAR Functional Units
 // Control/Branch, Scalar LS, Scalar ALU, Div/Mod, Mult, BF16 ALU, BF16 Mod/Div,
 // BF16 Mult, Type Conversion
+// 
+// TODO 
+// fix handshake timing
+// Fix parameterization log2 for banks[1:0] depends on num regs
+// Could not use for loops due to priority issue has to be dependent on previous if
 // ============================================================================
 `include "s_wb_arbiter_if.vh"
 `include "scalar_wb_pkg.vh"
 
 module s_wb_arbiter #(
-    parameter NUM_FUS   = 6,
+    parameter NUM_FUS   = 5,
     parameter NUM_BANKS = 4
 ) (
     input logic CLK, nRST,
     s_wb_arbiter_if.s_wb_arbiter vif
 ); 
     logic [NUM_FUS-1:0][NUM_BANKS-1:0] banks;
-    logic [NUM_FUS-1:0][NUM_BANKS-1:0] temp_banks;
 
     always_comb begin
         banks = '0;
@@ -36,63 +40,63 @@ module s_wb_arbiter #(
         //     vif.scalar_wb_out.ready[0] = 1'b0;
         // end
 
-        if (vif.scalar_wb_in.valid[1] && (!banks[0][vif.scalar_wb_in.rd[1][1:0]]) && vif.scalar_wb_in.maskornotscalar) begin
+        if (vif.scalar_wb_in.valid[1] && (!banks[0][vif.scalar_wb_in.rd[1][1:0]]&& vif.scalar_wb_in.maskornotscalar)) begin
             vif.scalar_wb_out.data[vif.scalar_wb_in.rd[1][1:0]]  = vif.scalar_wb_in.data[1];
             vif.scalar_wb_out.rd[vif.scalar_wb_in.rd[1][1:0]]    = vif.scalar_wb_in.rd[1];
             vif.scalar_wb_out.WEN[vif.scalar_wb_in.rd[1][1:0]]   = 1'b1;
-            temp_banks[1][vif.scalar_wb_in.rd[1][1:0]] = 1'b1;  
+            banks[1][vif.scalar_wb_in.rd[1][1:0]] = 1'b1;  
         end
         else if (vif.scalar_wb_in.valid[1] && banks[0][vif.scalar_wb_in.rd[1][1:0]]) begin
             vif.scalar_wb_out.ready[1] = 1'b0;
         end
-        banks[1] = temp_banks[1] | banks[0];
+        banks[1] = banks[1] | banks[0];
 
 
         if (vif.scalar_wb_in.valid[2] && (!banks[1][vif.scalar_wb_in.rd[2][1:0]])) begin
             vif.scalar_wb_out.data[vif.scalar_wb_in.rd[2][1:0]]  = vif.scalar_wb_in.data[2];
             vif.scalar_wb_out.rd[vif.scalar_wb_in.rd[2][1:0]]    = vif.scalar_wb_in.rd[2];
             vif.scalar_wb_out.WEN[vif.scalar_wb_in.rd[2][1:0]]   = 1'b1;
-            temp_banks[2][vif.scalar_wb_in.rd[2][1:0]] = 1'b1;
+            banks[2][vif.scalar_wb_in.rd[2][1:0]] = 1'b1;
             
         end
         else if (vif.scalar_wb_in.valid[2] && banks[1][vif.scalar_wb_in.rd[2][1:0]]) begin
             vif.scalar_wb_out.ready[2] = 1'b0;
         end
-        banks[2] = temp_banks[2] | banks[1];
+        banks[2] = banks[2] | banks[1];
 
 
         if (vif.scalar_wb_in.valid[3] && (!banks[2][vif.scalar_wb_in.rd[3][1:0]])) begin
             vif.scalar_wb_out.data[vif.scalar_wb_in.rd[3][1:0]]  = vif.scalar_wb_in.data[3];
             vif.scalar_wb_out.rd[vif.scalar_wb_in.rd[3][1:0]]    = vif.scalar_wb_in.rd[3];
             vif.scalar_wb_out.WEN[vif.scalar_wb_in.rd[3][1:0]]   = 1'b1;
-            temp_banks[3][vif.scalar_wb_in.rd[3][1:0]] = 1'b1;
+            banks[3][vif.scalar_wb_in.rd[3][1:0]] = 1'b1;
         end
         else if (vif.scalar_wb_in.valid[3] && banks[2][vif.scalar_wb_in.rd[3][1:0]]) begin
             vif.scalar_wb_out.ready[3] = 1'b0;
         end
-        banks[3] = temp_banks[3] | banks[2];
+        banks[3] = banks[3] | banks[2];
 
         if (vif.scalar_wb_in.valid[4] && (!banks[3][vif.scalar_wb_in.rd[4][1:0]])) begin
             vif.scalar_wb_out.data[vif.scalar_wb_in.rd[4][1:0]]  = vif.scalar_wb_in.data[4];
             vif.scalar_wb_out.rd[vif.scalar_wb_in.rd[4][1:0]]    = vif.scalar_wb_in.rd[4];
             vif.scalar_wb_out.WEN[vif.scalar_wb_in.rd[4][1:0]]   = 1'b1;
-            temp_banks[4][vif.scalar_wb_in.rd[4][1:0]] = 1'b1;
+            banks[4][vif.scalar_wb_in.rd[4][1:0]] = 1'b1;
         end
         else if (vif.scalar_wb_in.valid[4] && banks[3][vif.scalar_wb_in.rd[4][1:0]]) begin
             vif.scalar_wb_out.ready[4] = 1'b0;
         end
-        banks[4] = temp_banks[4] | banks[3];
+        banks[4] = banks[4] | banks[3];
 
         if (vif.scalar_wb_in.valid[5] && (!banks[4][vif.scalar_wb_in.rd[5][1:0]])) begin
             vif.scalar_wb_out.data[vif.scalar_wb_in.rd[5][1:0]]  = vif.scalar_wb_in.data[5];
             vif.scalar_wb_out.rd[vif.scalar_wb_in.rd[5][1:0]]    = vif.scalar_wb_in.rd[5];
             vif.scalar_wb_out.WEN[vif.scalar_wb_in.rd[5][1:0]]   = 1'b1;
-            temp_banks[5][vif.scalar_wb_in.rd[5][1:0]] = 1'b1;
+            banks[5][vif.scalar_wb_in.rd[5][1:0]] = 1'b1;
         end
         else if (vif.scalar_wb_in.valid[5] && banks[4][vif.scalar_wb_in.rd[5][1:0]]) begin
             vif.scalar_wb_out.ready[5] = 1'b0;
         end
-        banks[5] = temp_banks[5] | banks[4];
+        // banks[5] = banks[5] | banks[4];
 
         // if (vif.scalar_wb_in.valid[6] && (!banks[5][vif.scalar_wb_in.rd[6][1:0]])) begin
         //     vif.scalar_wb_out.data[vif.scalar_wb_in.rd[6][1:0]]  = vif.scalar_wb_in.data[6];
